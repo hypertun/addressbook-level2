19cbd62d61f1c2ade0cad64d473739e9381020a9
LAPTOP-80LTHV2A\Hypertun <hypertun@live.com>
2018-02-15 19:06:40 +0800

    Changed find command to case-insensitive, updated tests and User Guide.

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -17,7 +17,7 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
@@ -41,16 +41,24 @@ public class FindCommand extends Command {
     }
 
     /**
-     * Retrieves all persons in the address book whose names contain some of the specified keywords.
+     * Retrieves all persons in the address book whose names contain some of the specified keywords(case - insensitive).
      *
      * @param keywords for searching
      * @return list of persons found
      */
     private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords) {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
+        HashSet<String> keywordsConverted = new HashSet<>();
+        for(String lowerCaseKeyWords : keywords){
+            keywordsConverted.add(lowerCaseKeyWords.toLowerCase());
+        }
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
             final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
-            if (!Collections.disjoint(wordsInName, keywords)) {
+            final HashSet<String> wordsInNameConverted = new HashSet<>();
+            for(String lowerCasewordsInName : wordsInName){
+                wordsInNameConverted.add(lowerCasewordsInName.toLowerCase());
+            }
+            if (!Collections.disjoint(wordsInNameConverted, keywordsConverted)) {
                 matchedPersons.add(person);
             }
         }
